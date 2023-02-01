25356a248efd60ced7081c607ab3fce032d35dbf
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-02-01 16:42:41 +0100

    Esercizio Completato

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    
+    AppTitle
+  
+  },
+
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+
+  <AppTitle/>
+
+</template>
+
+<style scoped>
+
+</style>
